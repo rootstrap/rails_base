7699d4e16762ff87b1ee81f0c02ace26e331b985
fedeagripa <fagripa@rootstrap.com>
2017-10-12 11:14:19 -0300

    add code quality tools

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-require "bundler/setup"
+require 'bundler/setup'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
