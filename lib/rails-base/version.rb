aba73ee107826ef2b97a4ba1e0b4036018eea84b
fedeagripa <fagripa@rootstrap.com>
2017-10-12 11:14:19 -0300

    add code quality tools

--- lib/rails-base/version.rb
+++ lib/rails-base/version.rb
@@ -1,5 +1,5 @@
 module RailsBase
-  Version = VERSION = '0.0.0'
-  API_BASE_VERSION = '1.0.0'
-  REMOTE_URI = 'git://github.com/rootstrap/rails_api_base.git'
+  VERSION = '0.0.0'.freeze
+  API_BASE_VERSION = '1.0.0'.freeze
+  REMOTE_URI = 'git://github.com/rootstrap/rails_api_base.git'.freeze
 end
